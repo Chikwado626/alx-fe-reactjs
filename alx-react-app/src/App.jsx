c9c13ba3c41b5f704ab9b35833ca698e5d7d8c78
Chikwado626 <chikwadoezemma03@gmail.com>
2024-08-08 19:23:27 +0100

    App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessagee
+import WelcomeMessage from './components/WelcomeMessage
+import Header from './Header'
+import MainContent from './MainContent'
+import Footer from './Footer'
 
 function App() {
   return (
-    <>
+    <><Header />
+    <MainContent />
+     <Footer />
       <WelcomeMessage />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
