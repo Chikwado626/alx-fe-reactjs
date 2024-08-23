66c99706420bd88376b327f900f1e3c98df8d360
Chikwado626 <chikwadoezemma03@gmail.com>
2024-08-23 13:21:07 +0100

    components

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,6 +1,27 @@
-import { BrowserRouter, createBrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter,  Route, Routes } from "react-router-dom";
 
-createBrowserRouter
 BrowserRouter
 Route 
 Routes
+import Home from "./components/Home";
+import Services from "./components/Services";
+import About from "./components/About";
+import Home from "./components/Home";
+import Contact from "./components/Contacts";
+ 
+function layout () {
+        return (
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/services" element={<Services />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+            <Footer />
+          </Router>
+        );
+      }
+      
+      export default layout;
