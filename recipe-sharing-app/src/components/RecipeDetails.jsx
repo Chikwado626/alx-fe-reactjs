abe60b897a587520a402c6a199972bcd8dd25a94
Chikwado626 <chikwadoezemma03@gmail.com>
2024-08-29 22:26:07 +0100

    EditRecipeDetails.jsx

---
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -0,0 +1,20 @@
+ // RecipeDetails component
+ import { useRecipeStore } from './recipeStore';
+ import { Link, useParams } from 'react-router-dom';
+
+const RecipeDetails = () => {
+    const { recipeId } = useParams();
+
+ const RecipeDetails = ({ recipeId }) => {
+   const recipe = useRecipeStore(state =>
+     state.recipes.find(recipe => recipe.id === recipeId)
+   );
+
+   return (
+     <div>
+       <h1>{how do you do}</h1>
+       <p>{recipe.description}</p>
+       {/* Render EditRecipeForm and DeleteRecipeButton here */}
+     </div>
+   );
+ };
