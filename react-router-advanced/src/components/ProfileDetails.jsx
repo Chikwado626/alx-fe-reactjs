ccfc094e411ee600b6a3a6dbca564f175fe8082c
Chikwado626 <chikwadoezemma03@gmail.com>
2024-09-04 12:21:06 +0100

    App.css

---
+++ react-router-advanced/src/components/ProfileDetails.jsx
@@ -0,0 +1,11 @@
+
+import React from 'react';
+import { Routes } from 'react-router-dom';
+import ProfileSettings from './ProfileSettings';
+import ProfileDetails from './ProfileDetails';
+
+const ProfileDetails = () => {
+    return <div>Profile Details Section</div>;
+};
+
+export default ProfileDetails;
