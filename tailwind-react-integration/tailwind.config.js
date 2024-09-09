7c335922c5ed3537ea9b5fabed4a24f68ef8e2e4
Chikwado626 <chikwadoezemma03@gmail.com>
2024-09-09 15:06:23 +0100

    tailwind.config.js

--- tailwind-react-integration/tailwind.config.js
+++ tailwind-react-integration/tailwind.config.js
@@ -1,3 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false,
